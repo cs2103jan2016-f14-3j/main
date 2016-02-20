8248f2317983952474b0a9f14c9aad5dbd64b526
weilip1803@gmail.com <weilip1803@gmail.com>
2016-02-20 21:18:48 +0800

    Date and task holder objects

--- src/utils/Task.java
+++ src/utils/Task.java
@@ -1,5 +1,39 @@
 package utils;
 
-public class Task {
+import java.sql.Date;
+import java.text.DateFormat;
 
+public class Task {
+	private String title;
+	private String priority;
+	private CustomDate Startdate;
+	private CustomDate endDate;
+	public void Task(String title, String priority, CustomDate Startdate, CustomDate endDate) {
+		
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public CustomDate getStartdate() {
+		return Startdate;
+	}
+	public void setStartdate(CustomDate startdate) {
+		Startdate = startdate;
+	}
+	public CustomDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(CustomDate endDate) {
+		this.endDate = endDate;
+	}
+	
 }
