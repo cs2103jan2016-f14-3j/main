97e329be3bb4fd6453a8eb8c13e48a20a592c9b8
William <tanwcw@gmail.com>
2016-02-24 01:44:38 +0800

    Implemented a unit test
    checks whether output is correct if user inputs nothing inside.

---
+++ src/parser/ParserTest.java
@@ -0,0 +1,17 @@
+package parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParserTest {
+
+	Parser parser = new Parser();
+	
+	@Test
+	public void testExecuteCommandEmptyCommand() {
+		String result = parser.executeCommand("");
+		assertEquals(result,"invalid command format: ");
+	}
+
+}
