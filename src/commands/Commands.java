995a1ae9026d2aba0620cd435bdeb50b0f573138
wenhaohaoo <lwen.hao93@gmail.com>
2016-02-25 19:59:48 +0800

    Added basic commands

--- src/commands/Commands.java
+++ src/commands/Commands.java
@@ -2,4 +2,20 @@ package commands;
 
 public class Commands {
 
+	public static void addTask() {
+				
+	}
+	
+	public static void deleteTask() {
+		
+	}
+	
+	public static void updateTask() {
+		
+	}
+	
+	public static void readTask() {
+		
+	}
+	
 }
