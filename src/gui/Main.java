4ed1ec897d183da0dbd15fd5b41354ca0907a71e
Jorel <jorel_31@hotmail.com>
2016-03-04 01:02:56 +0800

    Error in JAVA API version. unable to run application directly from eclipse. To Be solved tmr

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,15 +1,26 @@
 package gui;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, (java.lang.String[])null);
+    }
+
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+            FXMLLoader.load(Main.class.getResource("POMPOM.fxml"));
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -19,8 +30,4 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
 	}
-}
