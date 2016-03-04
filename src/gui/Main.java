a0a404284f927d1f152e042ec1fcda0d8e145b82
weilip1803@gmail.com <weilip1803@gmail.com>
2016-03-04 20:03:33 +0800

    Try

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,26 +1,23 @@
 package gui;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("POMPOM.fxml"));
+        stage.setTitle("POMPOM");
+        stage.setScene(new Scene(root, 1000, 600));
+        stage.show();
+    }
 }
