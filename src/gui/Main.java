ec8a0beabd2be39857277c529eac071fab04a853
Jorel <jorel_31@hotmail.com>
2016-03-04 15:30:04 +0800

    add add

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -3,31 +3,21 @@ package gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Application.launch(Main.class, (java.lang.String[])null);
+        Application.launch(Main.class, args);
     }
-
-
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-            FXMLLoader.load(Main.class.getResource("POMPOM.fxml"));
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	}
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("POMPOM.fxml"));
+        stage.setTitle("POMPOM");
+        stage.setScene(new Scene(root, 1000, 600));
+        stage.show();
+    }
+}
