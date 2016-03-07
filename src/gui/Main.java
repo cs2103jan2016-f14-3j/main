ef3984f1cec503f1402371a8bf1545b9797c7a7b
Tay Kai Teng Jorel <a0126375@u.nus.edu>
2016-03-07 21:50:03 +0800

    merge

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,14 +1,25 @@
 package gui;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
 
 public class Main extends Application {
 
+	@FXML
+	Button fuckyou;
+	
+	@FXML
+	protected void hello(ActionEvent event){
+		System.out.println("bitch");
+	}
+	
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
