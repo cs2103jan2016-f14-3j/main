82427f0a7132710488edd0a3db18aa2b78ffc069
William <weechen@u.nus.com>
2016-03-09 01:03:17 +0800

    Updated invalidParser to prepare for future implementations

--- src/parser/InvalidParser.java
+++ src/parser/InvalidParser.java
@@ -1,7 +1,14 @@
 package parser;
 
+import command.Command;
+import command.EditCommand;
+
 public class InvalidParser {
 	public InvalidParser(String userCommand){
+	}
+	
+	public void executeCommand(){
+		//return new command(); 
 	}
 
 }
